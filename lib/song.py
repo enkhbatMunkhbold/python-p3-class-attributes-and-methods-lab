a2e3f69eb2ced9a32c69d65d90da908fa025cb2c
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-08-22 22:31:15 -0700

    Current status

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,29 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    
+
+    def __init__ (self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self. genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
