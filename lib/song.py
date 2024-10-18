27a6f4355984f8d543a4e9c589815e6545e3226d
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-10-18 08:11:25 -0700

    Complete Lab

--- lib/song.py
+++ lib/song.py
@@ -1,6 +1,7 @@
 class Song:
 
     count = 0
+    genre_count = {}
     genres = []
     artists = []
     
@@ -12,6 +13,8 @@ class Song:
         Song.add_song_to_count()
         Song.add_to_genres(self.genre)
         Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artist_count(self.artist)
 
     @classmethod
     def add_song_to_count(cls):
@@ -27,3 +30,18 @@ class Song:
         if artist not in cls.artists:
             cls.artists.append(artist)
 
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+# breakpoint()
